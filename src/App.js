705df8d016c5fb94045378cefc5af8ae7d198ec3
Osiris Román <osirisr1994@gmail.com>
2020-11-20 18:14:23 -0500

    Se añadió el componente Formulario.
    Este componente se encarga de permitirle al usuario decidir entre las
    diferentes opciones para escoger un seguro.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Header from './Componentes/Header/Header';
 
+import Formulario from './Componentes/Formulario/Formulario';
+
 import { Contenedor, ContenedorFormulario } from './App.styles';
 
 function App() {
   return (
     <Contenedor>
       <Header titulo='Cotizador de Seguros' />
+
+      <ContenedorFormulario>
+        <Formulario></Formulario>
+      </ContenedorFormulario>
     </Contenedor>
   );
 }
