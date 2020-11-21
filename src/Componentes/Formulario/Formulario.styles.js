69ca235dde3c23dfca1138d7c679a4c3c8f3b77d
Osiris Román <osirisr1994@gmail.com>
2020-11-20 20:06:03 -0500

    Añadiendo Styled Components al Formulario.

---
+++ src/Componentes/Formulario/Formulario.styles.js
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+export const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+export const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  --webkit-appearance: none;
+`;
+
+export const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
